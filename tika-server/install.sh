c5aba159559aff4f94c23e195e52db4c7d4875a3
Yi Zhang <yi.zhang@nlmatics.com>
2020-09-22 14:24:39 +0000

    change compiling path

--- tika-server/install.sh
+++ tika-server/install.sh
@@ -1,5 +1,5 @@
 mvn -fn -U clean install:install-file \
-   -Dfile=/Users/reshavabraham/work/nlm-tika/tika/tika-parsers/target3/tika-parsers/tika-parsers/2.0.5/tika-parsers-2.0.5.jar \
+   -Dfile=/home/yi_zhang/nlm/tika/tika-parsers/target3/tika-parsers/tika-parsers/2.0.5/tika-parsers-2.0.5.jar \
    -DgroupId=tika-parsers \
    -DartifactId=tika-parsers \
    -Dversion=2.0.5 \
